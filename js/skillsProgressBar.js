8265028e7b236de7cca4c7f7a17804836969fbf0
Teresa Rodríguez de Dios <45826126+terodedi99@users.noreply.github.com>
2022-03-18 09:49:38 +0100

    Update skillsProgressBar.js

--- js/skillsProgressBar.js
+++ js/skillsProgressBar.js
@@ -1,14 +1,14 @@
 var lang = {
     "java": "70%",
     "dart": "70%",
-    "typescript": "60%",
-    "javascript": "50%",
+    "typescript": "70%",
+    "javascript": "70%",
     "python": "50%",
 
     "prestashop": "70%",
-    "html": "60%",
-    "css": "60%",
-    "angular": "40%",
+    "html": "80%",
+    "css": "80%",
+    "angular": "60%",
 
     "flutter": "70%",
     "android": "50%",
@@ -16,8 +16,8 @@ var lang = {
     "espanol": "Nativo",
     "ingles": "B1",
 
-    "linux": "65%",
-    "latex": "50%",
+    "linux": "70%",
+    "latex": "80%",
 
     /*
     "mysql": "90%",
